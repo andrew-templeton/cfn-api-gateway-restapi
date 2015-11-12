c59d61825106c5aa6d3b20a6e3676f646eea78a1
Andrew Templeton <andrw.tmplton@gmail.com>
2015-11-11 19:59:15 -0600

    Upgraded dependency and made the resource simpler

--- index.js
+++ index.js
@@ -3,101 +3,94 @@ var AWS = require('aws-sdk');
 
 var CfnLambda = require('cfn-lambda');
 
-exports.handler = ApiGatewayRestApiHandler;
+var APIG = new AWS.APIGateway({apiVersion: '2015-07-09'});
 
-function ApiGatewayRestApiHandler(event, context) {
-  var ApiGatewayRestApi = CfnLambda({
-    Create: Create,
-    Update: Update,
-    Delete: Delete,
-    SchemaPath: [__dirname, 'schema.json'],
-    NoUpdate: NoUpdate
-  });
-  // Not sure if there's a better way to do this...
-  AWS.config.region = currentRegion(context);
-  var APIG = new AWS.APIGateway({apiVersion: '2015-07-09'});
-  
-  return ApiGatewayRestApi(event, context);
+exports.handler = CfnLambda({
+  Create: Create,
+  Update: Update,
+  Delete: Delete,
+  SchemaPath: [__dirname, 'schema.json'],
+  NoUpdate: NoUpdate
+});
 
+function Create(params, reply) {
+  APIG.createRestApi({
+    name: params.Name,
+    cloneFrom: params.BaseApiId,
+    description: params.Description
+  }, handleReply(reply));
+}
 
-  function Create(params, reply) {
-    APIG.createRestApi({
-      name: params.Name,
-      cloneFrom: params.BaseApiId,
-      description: params.Description
-    }, handleReply(reply));
+function Update(physicalId, params, oldParams, reply) {
+  // Full replace
+  if (params.BaseApiId !== oldParams.BaseApiId) {
+    return Delete(physicalId, null, function(deletionError) {
+      if (deletionError) {
+        return reply(deletionError);
+      }
+      Create(params, reply);
+    });
   }
+  var params = {
+    restApiId: physicalId,
+    patchOperations: []
+  };
 
-  function Update(physicalId, params, oldParams, reply) {
-    // Full replace
-    if (params.BaseApiId !== oldParams.BaseApiId) {
-      return Delete(physicalId, null, function(deletionError) {
-        if (deletionError) {
-          return reply(deletionError);
-        }
-        Create(params, reply);
-      });
+  ['Name', 'Description'].forEach(patch);
+
+  APIG.updateRestApi(params, handleReply(reply));
+
+  function patch(key) {
+    var keyPath = '/' + key.toLowerCase();
+    if (params[key] === oldParams[key]) {
+      return;
     }
-    var params = {
-      restApiId: physicalId,
-      patchOperations: []
-    };
-
-    ['Name', 'Description'].forEach(patch);
-
-    APIG.updateRestApi(params, handleReply(reply));
-
-    function patch(key) {
-      var keyPath = '/' + key.toLowerCase();
-      if (params[key] === oldParams[key]) {
-        return;
-      }
-      if (!oldParams[key]) {
-        return params.patchOperations.push({
-          op: 'add',
-          path: keyPath,
-          value: params[key]
-        });
-      }
-      if (!params[key]) {
-        return params.patchOperations.push({
-          op: 'remove',
-          path: keyPath
-        });
-      }
-      params.patchOperations.push({
-        op: 'replace',
+    if (!oldParams[key]) {
+      return params.patchOperations.push({
+        op: 'add',
         path: keyPath,
         value: params[key]
       });
     }
-  }
-
-  
-
-  function Delete(physicalId, params, reply) {
-    APIG.deleteRestApi({
-      restApiId: physicalId
-    }, function(err, data) {
-      // Already deleted, which is fine
-      if (!err || err.statusCode === 404) {
-        return reply();
-      }
-      reply(err.message);
+    if (!params[key]) {
+      return params.patchOperations.push({
+        op: 'remove',
+        path: keyPath
+      });
+    }
+    params.patchOperations.push({
+      op: 'replace',
+      path: keyPath,
+      value: params[key]
     });
   }
+}
 
-  function NoUpdate(old, fresh) {
-    return [
-      'Name',
-      'Description',
-      'BaseApiId'
-    ].every(function(key) {
-      return old[key] === fresh[key];
-    });
-  }
 
-};
+
+function Delete(physicalId, params, reply) {
+  APIG.deleteRestApi({
+    restApiId: physicalId
+  }, function(err, data) {
+    // Already deleted, which is fine
+    if (!err || err.statusCode === 404) {
+      return reply();
+    }
+    reply(err.message);
+  });
+}
+
+function NoUpdate(old, fresh) {
+  return [
+    'Name',
+    'Description',
+    'BaseApiId'
+  ].every(function(key) {
+    return old[key] === fresh[key];
+  });
+}
+
+
 
 function handleReply(reply) {
   return function(err, api) {
